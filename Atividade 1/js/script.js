50a413a290274e1e2d1a30e9f6204cb779ed1c40
lucasToniello <lucasgranja@hotmail.com>
2019-05-07 02:36:08 +0000

    Código de script atualizado

--- Atividade 1/js/script.js
+++ Atividade 1/js/script.js
@@ -1,4 +1,4 @@
-var rotacionar = function(camera, angulo){
+function rotacionar(camera, angulo){
 	var anguloRadianos = (angulo) * Math.PI / 180
 	var a = Math.sin(anguloRadianos) * camera.position.x * -1
 	var b = Math.cos(anguloRadianos) * camera.position.z
@@ -8,80 +8,48 @@ var rotacionar = function(camera, angulo){
 	camera.position.x = c + d
 	camera.position.z = a + b
 
-	camera.lookAt(0, 0, 0); // Tem que corrigir isso
+	camera.lookAt(scene.position);
 	camera.updateProjectionMatrix()
 }
 
-var container, stats;
-var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
-init();
-animate();
+function novaCamera(pi, pv){
+	var camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 500);
+	camera.position.set(pi[0], pi[1], pi[2]);
+	camera.lookAt(pv[0], pv[1], pv[2]);
+	return camera
+}
 
 function init() {
-container = document.createElement( 'div' );
-document.body.appendChild( container );
-camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	scene = new THREE.Scene();
+	camera = novaCamera([0, 0, 10], [0, 0, 0])
+	container = document.createElement('div');
+	renderer = new THREE.WebGLRenderer();
 
-scene = new THREE.Scene();
-var ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
-scene.add( ambientLight );
-// var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-// camera.add( pointLight );
-scene.add( camera );
-// model
-				var onProgress = function ( xhr ) {
-					if ( xhr.lengthComputable ) {
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
-					}
-				};
-				var onError = function () { };
-				THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
-				new THREE.MTLLoader()
-					.setPath( '../res/eyeball/' )
-					.load( 'eyeball.mtl', function ( materials ) {
-						materials.preload();
-						new THREE.OBJLoader()
-							.setMaterials( materials )
-							.setPath( '../res/eyeball/' )
-							.load( 'eyeball.obj', function ( object ) {
-								scene.add( object );
-							}, onProgress, onError );
-					} );
-				//
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-				//
-				window.addEventListener( 'resize', onWindowResize, false );
-				camera.position.set(0, 0, 10)
-			}
-			function onWindowResize() {
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-				renderer.setSize( window.innerWidth, window.innerHeight );
-			}
-			function onDocumentMouseMove( event ) {
-				mouseX = ( event.clientX - windowHalfX ) / 2;
-				mouseY = ( event.clientY - windowHalfY ) / 2;
-			}
-			//
-			function animate() {
-				requestAnimationFrame( animate );
-				rotacionar(camera, -1)
-				render();
-			}
-			function render() {
-				// camera.position.x += ( mouseX - camera.position.x ) * .05;
-				// camera.position.y += ( - mouseY - camera.position.y ) * .05;
-				camera.lookAt( scene.position );
-				renderer.render( scene, camera );
-			}
-		
+	document.body.appendChild(container);
+	container.appendChild(renderer.domElement);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	scene.add(camera);
+	scene.add(new THREE.AmbientLight(0xffffff, 2));
+
+	THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+	new THREE.MTLLoader().setPath('../res/eyeball/').load('eyeball.mtl', function(materials){
+		materials.preload();
+
+		new THREE.OBJLoader().setMaterials(materials).setPath('../res/eyeball/')
+		.load('eyeball.obj', function(object){
+			scene.add(object);
+		});
+	});
+				
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	rotacionar(camera, -1)
+	renderer.render(scene, camera);
+}
+
+var camera, scene, container, renderer;
+
+init();
+animate();
